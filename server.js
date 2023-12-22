e5e0727d6c2e58bdc25078d5ea92a823e2e72130
vien pham <vienpham@viens-Air.home>
2023-12-22 16:10:00 -0600

    a

---
+++ server.js
@@ -0,0 +1,12 @@
+const app = require("./src/app");
+
+require("dotenv").config();
+const PORT = process.env.PORT || 3055;
+
+app.listen(PORT, () => {
+  console.log(`WSV eCommerce start with port ${PORT}`);
+});
+
+// process.on("SIGINT", () => {
+//   server.close(() => console.log(`Exit Server Express`));
+// });
